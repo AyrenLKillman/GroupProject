11809f3f25caeba463186f7c73a3ac6f66d874c5
TLowe248 <105463078+TLowe248@users.noreply.github.com>
2024-11-15 12:54:29 -0600

    Added some text

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ThomasPageComponent } from './thomas-page/thomas-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "Thomas",
+		component: ThomasPageComponent
+	}
+];
---
+++ src/app/thomas-page/thomas-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thomas-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './thomas-page.component.html',
+  styleUrl: './thomas-page.component.css'
+})
+export class ThomasPageComponent {
+
+}
